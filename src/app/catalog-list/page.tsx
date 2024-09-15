33f0cc8e673456028d66d2b5d9afbcdbeb7aed49
normurodovm <nmaqsud302@gmail.com>
2024-09-15 20:29:56 +0500

    message

--- src/app/catalog-list/page.tsx
+++ src/app/catalog-list/page.tsx
@@ -1,24 +1,22 @@
 import { NextPage } from "next";
 
-
-
- interface CatalogType {
-  id:number;
-  img:string;
-  text:string;
-  name:string;
+interface CatalogType {
+  id: number;
+  img: string;
+  text: string;
+  name: string;
 }
 
-const CatalogList:NextPage<CatalogType> = ({ name }) => {
+const CatalogList: NextPage<CatalogType> = ({ name, img, text }) => {
   return (
     <div className="max-w-sm mx-auto p-4 border border-gray-300 rounded-lg shadow-lg">
+      <img src={img} alt={name} className="w-full h-48 object-cover rounded-md" />
       <h3 className="text-lg font-semibold py-4 hover:border-b-2 hover:border-primary transition duration-300 ease-in-out cursor-pointer">
         {name}
       </h3>
+      <p className="text-gray-600">{text}</p>
     </div>
   );
 };
 
-
-
-export default CatalogList
+export default CatalogList;
