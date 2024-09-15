b64ab3fb790c36209f5081a11cc498325264f5b1
normurodovm <nmaqsud302@gmail.com>
2024-09-15 20:09:59 +0500

    message

--- src/app/catalog-list/page.tsx
+++ src/app/catalog-list/page.tsx
@@ -1,6 +1,7 @@
 import { CatalogType } from "@/types/catalog-type";
+import { NextPage } from "next";
 
-export const CatalogList: React.FC<CatalogType> = ({ name }) => {
+export const CatalogList:NextPage<CatalogType> = ({ name }) => {
   return (
     <div className="max-w-sm mx-auto p-4 border border-gray-300 rounded-lg shadow-lg">
       <h3 className="text-lg font-semibold py-4 hover:border-b-2 hover:border-primary transition duration-300 ease-in-out cursor-pointer">
